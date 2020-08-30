19f4595a3ea8f36183534de78ed18f9e778c1676
Tyler Perry <tyler.pzizzle@gmail.com>
2020-08-30 18:31:57 +1000

    added attempt to display disks

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
+import 'dart:convert';
+
 import 'tether.dart';
+import 'dart:io';
 
 main() async {
   int count = 0;
   var t = await Tether.create(() {
     count++;
     print('resetting video! : $count');
+  });
+
+  //lsblk -o NAME,FSTYPE,MOUNTPOINT
+  //Process.run('ls', ['-l']).then((ProcessResult results) {
+  Process.run('lsblk', ['-o', 'NAME,FSTYPE,MOUNTPOINT'])
+      .then((ProcessResult results) {
+    String s = results.stdout as String;
+    s = s.replaceAll(RegExp(' +'), ' ');
+    var lines = LineSplitter().convert(s);
+    lines.removeWhere((e) => e.contains('UUID'));
+    lines.forEach((e) {
+      print('line: $e');
+      var ds = s.split(' ');
+      ds.forEach(print);
+    });
   });
 }
