61afaedceeb0f1c8bbff2668d0d9586ff6813a21
Tyler Perry <tyler.pzizzle@gmail.com>
2020-09-05 18:58:16 +1000

    added check for mounted disk before running

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,32 @@ main() async {
   //setup usb disks
   var disks = await getDisks();
   disks.forEach((element) => print(element));
-  var unmountedDisks = disks.where((e) {
-    return !e.isMounted();
-  }).toList();
-  //mount usb
-  if (unmountedDisks.length < 1) {
+  Disk mountedDisk =
+      disks.firstWhere((element) => element.mountpoint == '/mnt/usb');
+
+  if (mountedDisk == null) {
+    var unmountedDisks = disks.where((e) {
+      return !e.isMounted();
+    }).toList();
+    //mount usb
+    if (unmountedDisks.length < 1) {
+      return;
+    }
+    await unmountedDisks[0].mount('/mnt/usb');
+
+    var disks2 = await getDisks();
+    disks2.forEach((element) => print(element));
+    mountedDisk =
+        disks2.firstWhere((element) => element.mountpoint == '/mnt/usb');
+  }
+
+  if (mountedDisk == null) {
+    print('no usable disk found...');
     return;
   }
-  var mountedDisk = await unmountedDisks[0].mount('/mnt/usb');
-  print('disk : $mountedDisk');
-  var disk2 = await getDisks();
-  disk2.forEach((element) => print(element));
+  print('------');
+  print(mountedDisk.toString());
+
   //Handle tether to other raspberry pis
   int count = 0;
   var t = await Tether.create(() {
