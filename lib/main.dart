cd808a497c544512d08508132deebe650f767f86
Tyler Perry <tyler.pzizzle@gmail.com>
2020-09-05 17:37:47 +1000

    changed code order

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'getdisks.dart';
 import 'tether.dart';
 
 main() async {
+  //setup usb disks
+  var disks = await getDisks();
+  disks.forEach((element) => print(element));
+  //TODO: mount usb, find video
+
+  //Handle tether to other raspberry pis
   int count = 0;
   var t = await Tether.create(() {
     count++;
     print('resetting video! : $count');
+    //TODO: start video
   });
-
-  getDisks().then((value) => {value.forEach((element) => print(element))});
 }
